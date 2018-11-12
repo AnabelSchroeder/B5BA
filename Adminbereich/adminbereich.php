f9ea985364e426e32c46d2ba6eeecefdf5e83e79
AnabelSchroeder <anabel.schroeder@gmx.de>
2018-11-12 14:56:27 +0100

    Admin bereich grundlegende syntax angelegt

--- Adminbereich/adminbereich.php
+++ Adminbereich/adminbereich.php
@@ -1,22 +1,20 @@
 <?php
-//Programmierungshilfe muss rausgelöscht werden im verlauf
-$admin = true;
-
-
-echo"<div id=\"admin-main\">";
-    echo"<div id=\"admin-box\">";
+        //Stellt die Navbox in den Adminbereich-unterseiten dar
+        function AdminNavbox(){
+        //Programmierungshilfe muss rausgelöscht werden im verlauf
+        $admin = true;    
 
         //Seitennavigations-Box
         echo"<div id=\"admin-nav\">";
             //Mein Konto bereich
-            echo"<a class=\"admin-nav-ueberschrift\">Mein Konto</a><br>";
+            echo"<a class=\"admin-nav-ueberschrift\">Mein Konto</a><br><br>";
 
             if ($admin == true){
                 //User bereich
                 echo"<a class=\"admin-nav-ueberschrift\">User</a><br>"; 
                 echo"<a class=\"admin-nav-listenpunkt\">Kunde</a><br>";
                 echo"<a class=\"admin-nav-listenpunkt\">Admin</a><br>";
-                echo"<a class=\"admin-nav-listenpunkt\">Neu anlegen</a><br>";
+                echo"<a class=\"admin-nav-listenpunkt\">Neu anlegen</a><br><br>";
                 
                 //Artikelbereich
                 echo"<a class=\"admin-nav-ueberschrift\">Artikel</a><br>";
@@ -24,12 +22,27 @@ echo"<div id=\"admin-main\">";
             }
 
         echo"</div>";
+        }
+
+
+
 
         //Contentbereichs-Seiten
-        echo"<div  id=\"admin-contentseiten\">";
-        include "adminseiten.php";
-        echo"</div>";
 
-    echo"</div>";
-echo"</div>";    
+        //Startseite Des Adminbereichs
+        if ($seitenid == "Adminbereich"){
+        echo"<div id=\"admin-main\">";
+            echo"<div id=\"admin-box\">";    
+                AdminNavbox();
+                echo"<div  id=\"admin-contentseiten\">";
+        
+
+                echo"</div>";
+            echo"</div>";
+        echo"</div>";  
+        }
+
+
+
+  
 ?>
