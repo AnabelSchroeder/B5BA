bd52b592cc4a068d45de7e2076c384f08b0250bf
AnabelSchroeder <anabel.schroeder@gmx.de>
2018-11-12 13:58:27 +0100

    Fehler behebung Anzeige Adminbereich + Seiten dazu gefügt

--- Adminbereich/adminbereich.php
+++ Adminbereich/adminbereich.php
@@ -1,3 +1,35 @@
 <?php
+//Programmierungshilfe muss rausgelöscht werden im verlauf
+$admin = true;
 
+
+echo"<div id=\"admin-main\">";
+    echo"<div id=\"admin-box\">";
+
+        //Seitennavigations-Box
+        echo"<div id=\"admin-nav\">";
+            //Mein Konto bereich
+            echo"<a class=\"admin-nav-ueberschrift\">Mein Konto</a><br>";
+
+            if ($admin == true){
+                //User bereich
+                echo"<a class=\"admin-nav-ueberschrift\">User</a><br>"; 
+                echo"<a class=\"admin-nav-listenpunkt\">Kunde</a><br>";
+                echo"<a class=\"admin-nav-listenpunkt\">Admin</a><br>";
+                echo"<a class=\"admin-nav-listenpunkt\">Neu anlegen</a><br>";
+                
+                //Artikelbereich
+                echo"<a class=\"admin-nav-ueberschrift\">Artikel</a><br>";
+                echo"<a class=\"admin-nav-listenpunkt\">Neu anlegen</a><br>";
+            }
+
+        echo"</div>";
+
+        //Contentbereichs-Seiten
+        echo"<div  id=\"admin-contentseiten\">";
+        include "adminseiten.php";
+        echo"</div>";
+
+    echo"</div>";
+echo"</div>";    
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 echo"<!DOCTYPE html>";
 echo"<head>";
-echo"<meta charset="utf-8" />";
-echo"<meta name="viewport" content="width=device-width, initial-scale=1.0" />";
+echo"<meta charset=\"utf-8\" />";
+echo"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />";
 echo"<title>Pflanzenshop</title>";
 
 //Haupt CSS einbinden
-echo"<link rel="stylesheet" href="CSS/maincss.css" type="text/css">";
+echo"<link rel=\"stylesheet\" href=\"CSS/maincss.css\" type=\"text/css\">";
 //Adminbereich CSS einbinden
-echo"<link rel="stylesheet" href="CSS/admincss.css" type="text/css">";
+echo"<link rel=\"stylesheet\" href=\"CSS/admincss.css\" type=\"text/css\">";
 
 echo"</head>";
 
@@ -16,9 +16,24 @@ echo"</head>";
 echo"<body>";
 
 
+// der Button muss später in den Header
+echo"<form action=\"index.php\" method=\"get\">";
+echo"<button name=\"Seiten_ID\" type=\"submit\" value=\"Adminbereich\">Admin</button>";
+echo"</form>";
+
+
+if(isset($_GET['Seiten_ID'])) {
+    $seitenid = $_GET['Seiten_ID'];
+} else {
+    $seitenid = "index";
+}
+
 
 //Adminbereich einbinden
-include (Adminbereich/adminbereich.php);
+if ($seitenid == "Adminbereich"){
+    include "Adminbereich/adminbereich.php";
+    
+}
 
 echo"</body>";
 
