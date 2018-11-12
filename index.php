9f0bde1c543946492018cf9be3949795833256f7
AnabelSchroeder <anabel.schroeder@gmx.de>
2018-11-12 11:28:17 +0100

    ertellung des Main PHP und einbindung von css

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
+echo"<!DOCTYPE html>";
+echo"<head>";
+echo"<meta charset="utf-8" />";
+echo"<meta name="viewport" content="width=device-width, initial-scale=1.0" />";
+echo"<title>Pflanzenshop</title>";
+
+//Haupt CSS einbinden
+echo"<link rel="stylesheet" href="CSS/maincss.css" type="text/css">";
+//Adminbereich CSS einbinden
+echo"<link rel="stylesheet" href="CSS/admincss.css" type="text/css">";
+
+echo"</head>";
+
+
+echo"<body>";
+
+
+
+//Adminbereich einbinden
+include (Adminbereich/adminbereich.php);
+
+echo"</body>";
+
+
+
+echo"</html>";
 
 ?>
