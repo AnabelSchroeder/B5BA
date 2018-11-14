57c37334a83dc851b938c459022a725fe0b06619
Lisa Peters <lisa_peters@Lisas-MBP-2.fritz.box>
2018-11-14 17:45:07 +0100

    Frontend-Ordner und Platzhalterdateien angelegt

---
+++ Frontend/footer.php
@@ -0,0 +1,83 @@
+<?php
+///////////////////////////////////////
+// footer des Webshops (BA WS 18/19) //
+// Ersteller: Lisa Peters //
+///////////////////////////////////////
+
+echo "<footer>";
+
+    // Logo anzeigen
+    echo "<img src=\"../img/PH.jpg\">";
+
+    //Container für interne Links
+    echo "<div>";
+
+        // Verlinkung zur Home-Seite anzeigen
+        echo "<button>";
+            echo "Home";
+        echo "</button>";
+
+        // Verlinkung zur Shop-Seite anzeigen
+        echo "<button>";
+            echo "Shop";
+        echo "</button>";
+
+        // Verlinkung zum Impressum
+        echo "<button>";
+            echo "Impressum";
+        echo "</button>";
+
+        //Verlinkung zur Datenschutzerklärung
+        echo "<button>";
+            echo "Datenschutzerklärung";
+        echo "</button>";
+
+    echo "</div>";
+
+    // Container für externe Links
+    echo "<div>";
+
+        // Facebook - Logo & Link
+        echo "<button>";
+            echo "<img src=\"../img/PH.jpg\">";
+            echo "Facebook";
+        echo "<button>";
+
+        // Twitter - Logo & Link
+        echo "<button>";
+            echo "<img src=\"../img/PH.jpg\">";
+            echo "Twitter";
+        echo "<button>";
+
+        // Instagram
+        echo "<button>";
+            echo "<img src=\"../img/PH.jpg\">";
+            echo "Instagram";
+        echo "<button>";
+
+    echo "</div>";
+
+    // Container für Firmenangaben
+    echo "<div>";
+
+        // Name & Adresse
+        echo "<div>";
+            echo "Muster Unternehmen, Musterstrasse, Musterort";
+        echo "<div>";
+
+        // Tel
+        echo "<div>";
+            echo "Telefon: 01234-56789";
+        echo "</div>";
+
+        // mail
+        echo "<div>";
+            echo "mail@musterunternehmen.com";
+        echo "</div>";
+
+
+    echo "</div>";
+
+echo "</footer>";
+
+?>
---
+++ Frontend/header.php
@@ -0,0 +1,41 @@
+<?php
+///////////////////////////////////////
+// header des Webshops (BA WS 18/19) //
+// Ersteller: Lisa Peters //
+///////////////////////////////////////
+
+echo "<header>";
+    // Logo anzeigen
+    echo "<img src=\"../img/PH.jpg\">";
+
+    // Verlinkung zur Home-Seite anzeigen
+    echo "<button>";
+        echo "Home";
+    echo "</button>";
+
+    // Verlinkung zur Shop-Seite anzeigen
+    echo "<button>";
+        echo "Shop";
+    echo "</button>";
+
+    // Icon für Suchfeld
+    echo "<button>";
+        echo "<img src=\"../img/PH.jpg\">";
+    echo "</button>";
+
+    // Icon für Account/Login
+    echo "<button>";
+        echo "<img src=\"../img/PH.jpg\">";
+    echo "</button>";
+
+    // Icon für Warenkorb mit Artikelanzahlanzeige
+    echo "<button>";
+        echo "<img src=\"../img/PH.jpg\">";
+        echo "<div id=\"warenkorb_art_anz\">";
+            echo "";
+        echo "</div>";
+    echo "</button>";
+
+echo "</header>";
+
+?>
