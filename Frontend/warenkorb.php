9e9ec79bb50a2d83b7950224bb9c2ddefedbe4b3
Lisa Peters <lisa_peters@Lisas-MBP-2.fritz.box>
2018-11-19 12:21:21 +0100

    +grob Aufbau Warenkorb

--- Frontend/warenkorb.php
+++ Frontend/warenkorb.php
@@ -4,6 +4,17 @@
 // Ersteller: Lisa Peters //
 ///////////////////////////////////////
 
+echo "<h1>Warenkorb</h1>";
+// Schleife zum anzeigen der Artikel
+echo "<div>";
+echo "<img src=\"../assets/PH.jpg\">";
+echo "...</div>";
+//...
+
+echo "<div>";
+//
+echo "</div>";
+
 
 
 ?>
