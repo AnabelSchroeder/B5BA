74047e23230aa0917233198a18ec0f2c75272258
Lisa Peters <lisa_peters@Lisas-MBP-2.fritz.box>
2018-11-26 00:59:45 +0100

    L: header.php - Bildpfad angepasst

--- Frontend/header.php
+++ Frontend/header.php
@@ -20,7 +20,7 @@ echo "<div id=\"L_headerLinks\">";
     echo "<form action=\"index.php\" method=\"get\">";
         echo "<button class=\"L_headerButton\" name=\"Seiten_ID\" type=\"submit\" ";
                 echo "value=\"index\">";
-            echo "<img class=\"L_img\" src=\"../assets/PH.jpg\">";
+            echo "<img class=\"L_img\" src=\"assets/PH.jpg\">";
         echo "</button>";
     echo "</form>";
 
@@ -75,7 +75,7 @@ echo "<div id=\"L_headerRechts\">";
    /* echo "<form action=\"index.php\" method=\"get\">";
         echo "<button name=\"Seiten_ID\" type=\"submit\" ";
                 echo "value=\"?\">";
-            echo "<img class=\"L_img\" src=\"../assets/PH.jpg\">";
+            echo "<img class=\"L_img\" src=\"assets/PH.jpg\">";
         echo "</button>";
     echo "</form>"; 
     
@@ -86,7 +86,7 @@ echo "<div id=\"L_headerRechts\">";
                         name=\"header_search\" placeholder=\"Suche\" onkeyup=\"L_startSearch()\">";
     echo "</form>"; 
         echo "<button onclick=\"L_showSearchField()\" class=\"L_headerButton\" >";
-            echo "<img class=\"L_img\" src=\"../assets/PH.jpg\">";
+            echo "<img class=\"L_img\" src=\"assets/PH.jpg\">";
         echo "</button>";
 
         
@@ -98,7 +98,7 @@ echo "<div id=\"L_headerRechts\">";
     echo "<form action=\"index.php\" method=\"get\">";
         echo "<button class=\"L_headerButton\" name=\"Seiten_ID\" type=\"submit\" ";
                 echo "value=\"Login\">";
-            echo "<img class=\"L_img\" src=\"../assets/PH.jpg\">";
+            echo "<img class=\"L_img\" src=\"assets/PH.jpg\">";
         echo "</button>";
     echo "</form>";
 
@@ -110,7 +110,7 @@ echo "<div id=\"L_headerRechts\">";
     echo "<form action=\"index.php\" method=\"get\">";
         echo "<button class=\"L_headerButton\" name=\"Seiten_ID\" type=\"submit\" ";
                 echo "value=\"Warenkorb\">";
-            echo "<img class=\"L_img\" src=\"../assets/PH.jpg\">";
+            echo "<img class=\"L_img\" src=\"assets/PH.jpg\">";
             echo "<div id=\"L_warenkorb_art_anz\">";
                 echo /*$w_art_anz*/ "ZAHL";
             echo "</div>";
