0e9b4b4cb7d6927dc434d19e47042e4ed09f6702
antoniaschwarz <antonia.schwarz@hotmail.de>
2018-11-26 20:37:41 +0100

    datenbank verbindung läuft

--- Pflanzenshop/registrierung.php
+++ Pflanzenshop/registrierung.php
@@ -1,12 +1,10 @@
 <?php
 /*nutzer daten vom registrieren in die Datenbank aufnehmen */
 
-if (isset ($_POST['user_Registration_speichern']))
+if (isset ($_POST['user_registration_speichern']))
 {
     $sql = "INSERT INTO nutzer
             (
-               // n_id,
-                admin,
                 n_nname,
                 n_vname,
                 n_strasse,
@@ -22,19 +20,22 @@ if (isset ($_POST['user_Registration_speichern']))
             
             VALUES
             (
-                false,
+                
                 \"".$_POST['user_registrieren_nname']."\",
                 \"".$_POST['user_registrieren_vname']."\",
                 \"".$_POST['user_registrieren_strasse']."\",
                 \"".$_POST['user_registrieren_ort']."\",
                 \"".$_POST['user_registrieren_plz']."\",
                 \"".$_POST['user_registrieren_mail']."\",
-                \"".$_POST['user_registrieren_login_namr']."\",
+                \"".$_POST['user_registrieren_login_name']."\",
                 \"".$_POST['user_registrieren_passwort']."\",
                 false,
-                \"".$_POST['user_registrieren_zahlart']."\",
+                \"".$_POST['user_registrieren_zahlart']."\"
             );";
-    mysqli_query($verbinde, $sql) OR die (mysqli_error());
+    $sql = mysqli_query($verbinde, $sql) OR die (mysqli_error());
+
+
+    echo "Daten egspeichert";
 }
 
 
