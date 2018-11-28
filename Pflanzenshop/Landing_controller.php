086b37ffea07a58a71bfe288715b6f3afb654f22
antoniaschwarz <antonia.schwarz@hotmail.de>
2018-11-28 10:30:51 +0100

    4 neueste artikel  aus datenbank selektieren

--- Pflanzenshop/Landing_controller.php
+++ Pflanzenshop/Landing_controller.php
@@ -14,70 +14,31 @@
 //landing_page
 //artikel neuheiten anzeigen
 
-$sql = "SELECT art_id, art_name, art_preis, art_bild FROM artikel WHERE art_id =1;";
-$sql= mysqli_query($verbinde, $sql);
+//informationen aud Datenbank ziehen
+$sql = "SELECT art_id, art_name, art_preis, art_bild FROM artikel ORDER BY art_id DESC LIMIT 0,4;";
+$result = mysqli_query($verbinde, $sql);
 
-if (mysqli_num_rows ($sql) > 0)
+if (mysqli_num_rows ($result) > 0)
 {
-    while ($zeile = mysqli_fetch_assoc($sql))
+    while ($zeile = mysqli_fetch_assoc($result))
     {
-        echo $zeile['art_name'];
-        echo "<br>";
-        echo $zeile['art_preis'];
+       
+//für jeden der 4 artikel einen div container mit bild, titel und preis ausgeben
+        echo "<div class=\"landing_neuheiten_artikel\">";
+            $art_id = $zeile['art_id'];
+            echo "<button name=\"Seiten_ID\"   type=\"submit\" value=\"shop\">
+            <img class=\"landing_artikel_bild\" src=\"img/".$zeile['art_bild']."\">";
+            echo "<span class=\"landing_unterschrift\">".$zeile['art_name']."</span><br>";
+             echo "<span>".$zeile['art_preis']." € </span>";
+        echo "</button></div>";
     }
 }
-
+// rückmeldung bei fehler
 else{
     echo "nope";
 }
-
-
-/*
-$neueste_artikel = "SELECT art_id, art_name, art_preis FROM artikel LIMIT".$anzahl." -4, 4; ";
-for ($i=0; $i<=4; $i++)
-{
-    echo "<div class=\"landing_neuheiten_artikel\">";
-    echo "<img class=\"landing_artikel_bild\" src=\"img/schwertfarn.jpg\">";
-    echo "<span class=\"landing_unterschrift\">".$neueste_artikel['art_name']." </span><br>";
-    echo "<span>".$_neueste_artikel['art_preis']."€ </span>";
-echo "</div>";
-}
+   
 
 
 
-
-/*
-$landing_artikel_1_titel
-
-$landing_artikel_1_preis
-
-
-
-
-
-$landing_artikel_1_titel
-
-$landing_artikel_1_preis
-
-
-
-
-
-$landing_artikel_1_titel
-
-$landing_artikel_1_preis
-
-
-
-
-$landing_artikel_1_titel
-
-$landing_artikel_1_preis
-
-
-if (isset ($_POST['User_Registration_Speichern']))
-{
-    
-}
-*/
 ?>
