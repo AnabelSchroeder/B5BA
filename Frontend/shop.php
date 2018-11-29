6d822c95b2d1f99ac3ba770b97cc858718f88b1a
Lisa Peters <lisa_peters@Lisas-MBP-2.fritz.box>
2018-11-29 16:59:01 +0100

    L: Filteranzeige mit Preisfilter in 1 div; Kommentierung

--- Frontend/shop.php
+++ Frontend/shop.php
@@ -8,8 +8,10 @@
 //Haupt CSS einbinden
 echo"<link rel=\"stylesheet\" href=\"../CSS/maincss.css\" type=\"text/css\">";
 
-// Bildbereich mit Suchfeld
+//_Bildbereich mit Suchfeld______________________________________
+    //Bild-Container
 echo "<div id=\"L_Content_SucheContainer\">";
+        //Formular für's Suchfeld mit Button
     echo "<form action=\"index.php\" method=\"post\">";
         echo "<input id=\"L_ContSuchfeld\" type=\"text\" 
                     name=\"L_searchfield\" onkeyup=\"L_startSearch()\" 
@@ -21,55 +23,56 @@ echo "<div id=\"L_Content_SucheContainer\">";
 echo "</div>";
 
 
-// Filterleiste
+// Filterleiste _________________________________________________
 echo "<div id=\"L_FilterBarCon\">";
+        //Container für den Content der Filterleiste
+    echo "<div id=\"L_FilterBar\">";
+            //Container für Preisfilterfeld und gewählte Filteranzeige
+        echo "<div>";
+            echo "<div id=\"L_FilterPreis\" class=\"L_FilterKat\">";
+                echo "<span>Preis</span>";
+                echo "<button onclick=\"L_showPreisFilterMenu()\">";
+                    echo "<img class=\"L_FilterButtonIcon\" src=\"../assets/PH.jpg\">";
+                echo "</button>";
+            echo "</div>";
+            echo "<div id=\"L_gewFilterAnzeige\">";
+                echo "Gewählte Filter: ";
+            echo "</div>";
+        echo "</div>";
 
-echo "<div id=\"L_FilterBar\">";
+            //Filter für Farbe
+        echo "<div id=\"L_FilterFarbe\" class=\"L_FilterKat\">";
+            echo "<span>Farbe</span>";
+            echo "<button onclick=\"L_showFarbeFilterMenu()\">";
+                echo "<img class=\"L_FilterButtonIcon\" src=\"../assets/PH.jpg\">";
+            echo "</button>";
+        echo "</div>";
 
-    echo "<div id=\"L_FilterPreis\" class=\"L_FilterKat\">";
-        echo "<span>Preis</span>";
-        echo "<button onclick=\"L_showPreisFilterMenu()\">";
-            echo "<img class=\"L_FilterButtonIcon\" src=\"../assets/PH.jpg\">";
-        echo "</button>";
+            //Filter für Kategorie
+        echo "<div id=\"L_FilterKategorie\" class=\"L_FilterKat\">";
+            echo "<span>Kategorie</span>";
+            echo "<button onclick=\"L_showKategorieFilterMenu()\">";
+                echo "<img class=\"L_FilterButtonIcon\" src=\"../assets/PH.jpg\">";
+            echo "</button>";
+        echo "</div>";
+
+            //Filter für Anlässe
+        echo "<div id=\"L_FilterTopic\" class=\"L_FilterKat\">";
+            echo "<span>Anlässe und Themen</span>";
+            echo "<button onclick=\"L_showTopicFilterMenu()\">";
+                echo "<img class=\"L_FilterButtonIcon\" src=\"../assets/PH.jpg\">";
+            echo "</button>";
+        echo "</div>";
+
+    echo "<form action=\"index.php\" method=\"post\">";
+            //Zurücksetzenbutton
+        echo "<button id=\"L_filterReset\" type=\"reset\">Filter zurücksetzen</button>";
+
+    echo "</form>";
+
     echo "</div>";
-
-
-    echo "<div id=\"L_FilterFarbe\" class=\"L_FilterKat\">";
-        echo "<span>Farbe</span>";
-        echo "<button onclick=\"L_showFarbeFilterMenu()\">";
-            echo "<img class=\"L_FilterButtonIcon\" src=\"../assets/PH.jpg\">";
-        echo "</button>";
+   
     echo "</div>";
-
-
-    echo "<div id=\"L_FilterKategorie\" class=\"L_FilterKat\">";
-        echo "<span>Kategorie</span>";
-        echo "<button onclick=\"L_showKategorieFilterMenu()\">";
-            echo "<img class=\"L_FilterButtonIcon\" src=\"../assets/PH.jpg\">";
-        echo "</button>";
-    echo "</div>";
-
-
-    echo "<div id=\"L_FilterTopic\" class=\"L_FilterKat\">";
-        echo "<span>Anlässe und Themen</span>";
-        echo "<button onclick=\"L_showTopicFilterMenu()\">";
-            echo "<img class=\"L_FilterButtonIcon\" src=\"../assets/PH.jpg\">";
-        echo "</button>";
-    echo "</div>";
-
-echo "<form action=\"index.php\" method=\"post\">";
-
-    echo "<button id=\"L_filterReset\" type=\"reset\">Filter zurücksetzen</button>";
-
-echo "</form>";
-
-echo "</div>";
-
-echo "<div id=\"L_FilterAnzeige\">";
-    echo "BLABLABLA";
-echo "</div>";
-
-echo "</div>";
 
 
 // Artikelanzeige (der Suche)
@@ -92,7 +95,7 @@ echo "</div>";
 echo "<div id=\"L_shopPagContBack\">";
     echo "<div id=\"L_shopPagContSigns\">";
         
-            echo "<img src=\"../assets/PH.jpg\">";
+            echo "<img class=\"L_img\" src=\"../assets/PH.jpg\">";
         
             echo "<div id=\"L_shopPagAusgabe\">";
 
@@ -100,7 +103,7 @@ echo "<div id=\"L_shopPagContBack\">";
 
             echo "</div>";
 
-            echo "<img src=\"../assets/PH.jpg\">";
+            echo "<img class=\"L_img\" src=\"../assets/PH.jpg\">";
             
     echo "</div>";
 echo "</div>";
