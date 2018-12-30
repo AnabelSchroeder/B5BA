6187d611310c7c5bdb339260910ec94892df8db8
Lisa Peters <lisa_peters@Lisas-MBP-2.fritz.box>
2018-12-30 01:42:38 +0100

    Funktion WarenkorbArtAnz included

--- Frontend/header.php
+++ Frontend/header.php
@@ -118,27 +118,5 @@ echo "</header>";
 
 /*echo "<script type=\"text/javascript\" src=\"../js/L.js\"></script>";*/
 
-function warenkorbAnzahl(){
-    $servername = "localhost";
-$username = "root";
-$passwort = "root";
-$dbname = "BA_webshop"; //in ba_webshop ändern bei zusammenführung
-
-//$port = 8889;
-
-//$link = mysqli_init();
-//$success = mysqli_real_connect($link, $servername, $username, $passwort, $dbname, $port);
-
-$verbinde = mysqli_connect($servername, $username, $passwort);
-$connmysql = mysqli_select_db($verbinde, $dbname);
-
-$L_cookie = $_COOKIE['KEINEAHNUNG']; //!!!!!!!!!
-
-$L_sqlCookieId = "SELECT cookie_id FROM cookie WHERE cookie_wert='$L_cookie'"; 
-$L_cookieId = mysqli_query($verbinde, $L_sqlCookieId);
-
-$L_sqlbasketCount = "SELECT COUNT (korb_id) FROM warenkorb WHERE cookie_id='$L_cookieId'";
-$L_basketCount = mysqli_query($verbinde, $L_sqlbasketCount);
-return $L_basketCount;
-}
+include "Frontend/L_DB_Warenkorbanzahl.php";
 ?>
