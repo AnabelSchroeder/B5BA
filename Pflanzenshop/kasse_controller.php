a6b121248bfdadaec5120904f27b130c19d85916
antoniaschwarz <antonia.schwarz@hotmail.de>
2019-01-01 19:52:05 +0100

    expire prüfung und artikelzahl

--- Pflanzenshop/kasse_controller.php
+++ Pflanzenshop/kasse_controller.php
@@ -11,8 +11,10 @@
 
 
 
-//überprüfung, ob eingeloggt
-$sql ="SELECT logged_in FROM cookie WHERE cookie_wert=\"".$_COOKIE['sid']."\";";
+//überprüfung, ob eingeloggt//////////////////////////////////////////////////////////////
+if ($seitenid== "kasse_1" || "kasse_2" || "kasse_3" || "kasse_4")
+{
+$sql ="SELECT logged_in, expire  FROM cookie WHERE cookie_wert=\"".$_COOKIE['sid']."\";";
 $result = mysqli_query($verbinde, $sql);
 
 if (mysqli_num_rows ($result) > 0)
@@ -20,29 +22,51 @@ if (mysqli_num_rows ($result) > 0)
             while ($zeile = mysqli_fetch_assoc($result))
             {
                 $logged_in = $zeile['logged_in'];
+                $expire = $zeile['expire'];
+                $time_aktuell = time();
                 
             }
         }
-//weiterleitung zu login wenn nicht eingeloggt
+//nicht eingeloggt
+//weiterleitung zu login wenn nicht eingeloggt////////////////////////////////////////////
 
 
 
-   if ($logged_in == false)
+   if ($logged_in == false  AND $seitenid =="kasse_1")
     {
         echo "nicht eingeloggt";
-        $seitenid ='login';
-       // echo "<script> alert(\"Bitte einloggen um fortzufahren\")</script>";
-        //header("Location:index.php?Seiten_ID=login");
+  
+        header("Location:index.php?Seiten_ID=login");
     }
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 
+//eingeloggt
+
+//expire überprüfen: abgelaufen:
+if ($logged_in == true AND $expire < $time_aktuell AND $seitenid =="kasse_1" )
+    {
+       $sql = "UPDATE cookie
+       SET logged_in = false
+       WHERE cookie_wert=\"".$_COOKIE['sid']."\";";
+       $result = mysqli_query($verbinde, $sql);
+  
+        header("Location:index.php?Seiten_ID=login");
+    }
 
 // Nutzerdaten zur Nutzerid (übermittelt vom Login) selektieren
-    if ($logged_in==true )
-  // else
+
+
+
+    if ($logged_in==true AND $expire > $time_aktuell)
+ 
 //cookie id ermitteln
 {
-$sql ="SELECT cookie_id FROM cookie WHERE cookie_wert =\"".$_COOKIE['sid']."\";";
+
+
+//user daten selektieren
+
+$sql ="SELECT cookie_id, n_id FROM cookie WHERE cookie_wert =\"".$_COOKIE['sid']."\";";
 $result = mysqli_query($verbinde, $sql);
          
         if (mysqli_num_rows ($result) > 0)
@@ -50,6 +74,7 @@ $result = mysqli_query($verbinde, $sql);
             while ($zeile = mysqli_fetch_assoc($result))
             { 
                 $cookie_id=$zeile['cookie_id'];
+                $kasse_n_id = $zeile['n_id'];
             }
         }
 
@@ -57,11 +82,8 @@ $result = mysqli_query($verbinde, $sql);
 
 
 
-       //zu testzwecken feste id
-       $sql = "SELECT * FROM nutzer WHERE n_id=3";
-
-
-      //  $sql = "SELECT * FROM nutzer WHERE n_id=\"".$nutzer."\";";
+      //nutzerdaten selektieren
+       $sql = "SELECT * FROM nutzer WHERE n_id=\"".$kasse_n_id."\";";
         $result = mysqli_query($verbinde, $sql);
          
         if (mysqli_num_rows ($result) > 0)
@@ -69,14 +91,17 @@ $result = mysqli_query($verbinde, $sql);
             while ($zeile = mysqli_fetch_assoc($result))
             {
                 
- //sperre abprüfen
+ //sperre abprüfen//////////////////////////////////////////////////////////////
                 $sperre=$zeile['n_sperre'];
+
+//gesperrt:
 // benachrichtigung und weiter button disablen
-                if($sperre==true)
+                if($sperre== 1)
                 {
                     $K_Fehler = "Sie sind derzeit gesperrt. Ein Bestellvorgang ist nicht möglich!";
-                    echo ("<script type=\"text/javascript\"> kasse_sperren(); </script>");
+                    echo "<script type=\"text/javascript\"> kasse_sperren(); </script>";
                 }
+//////////////////////////////////////////////////////////////////////////////
                 //Name und Adresse in Variablen speichern
                 $kasse_vname= $zeile['n_vname'];
                
@@ -89,9 +114,26 @@ $result = mysqli_query($verbinde, $sql);
 
                 $kasse_zahlart = $zeile['n_zahlart'];
             }
-        }
+    } 
     
-        
+//warenkorbdaten selektieren für anzahl und gesamtbetrag
+$sql= "SELECT korb_id FROM warenkorb WHERE cookie_id=\"".$cookie_id."\";";
+$result = mysqli_query($verbinde, $sql);
+         
+if (mysqli_num_rows ($result) > 0)
+{   
+    //$resultset = array();
+    while ($wkzeile = mysqli_fetch_array($result)) 
+      //einträge zählen
+      $wk_art_anzahl= count ($wkzeile); 
+        {
+      /* artikel daten zu den artikel IDs aus dem warenkorb suchen
+        $sql = "SELECT  art_preis
+        FROM artikel WHERE art_id=\"".$wkzeile['art_id']."\";";
+        $result = mysqli_query($verbinde, $sql);*/
+        }
+    }
+}
 /////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
     // kasse2-zahlart: neue zahlart in Datenbank speichern
@@ -112,7 +154,7 @@ if($seitenid == "kasse_3")
 {
     
     
-// kasse 3: artikel stückzahl erhöhen    
+// kasse 3: artikel stückzahl erhöhen ///////////////////////////////////////////////////////   
 if (isset($_POST['kasse_3_anzahl_up']))
 {
     // erhöhen, wenn anzahl kleiner als artikel stückzahl
@@ -130,7 +172,7 @@ if (isset($_POST['kasse_3_anzahl_up']))
     }
 } 
 
-// kasse3: artikel stückzahl verringern
+// kasse3: artikel stückzahl verringern////////////////////////////////////////////////////////////
 if (isset($_POST['kasse_3_anzahl_down']))
 {
     if ($_POST['kasse_wk_zahl']>1)
@@ -143,7 +185,7 @@ if (isset($_POST['kasse_3_anzahl_down']))
 
 } 
 
-//artikel aus warenkorb löschen
+//artikel aus warenkorb löschen/////////////////////////////////////////////////////////////////////////
 if (isset($_POST['kasse_3_artikel_delete']))
 {
     $sql = "DELETE FROM warenkorb
@@ -152,34 +194,46 @@ if (isset($_POST['kasse_3_artikel_delete']))
 } 
 
 
-///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// warenkorb daten selektieren
+// warenkorb daten selektieren/////////////////////////////////////////////////////////////
+    //ausgabe nav zeile
+    echo "<div class=\"kasse_headleiste\">";
+    echo "Kasse <br>";
+    echo "<a class=\"kasse_nav\">Adresse </a> <a class=\"kasse_nav\"> Zahlungsmethode </a> <a class=\"kasse_nav_active\"> Kauf abschließen </a> <a class=\"kasse_nav\"> Bestellbestätigung </a>";
+echo "</div>";
+
+    //linke seite
+    echo "<div class=\"kasse_div\">";
+    echo "<div class=\"kasse_links\">";
+        echo "<span class=\"kasse_ueberschrift_klein\">Überprüfe deine Bestellung und schließe den Kauf ab:</span><br>";
+        echo "<table>";
+
+
 $sql= "SELECT * FROM warenkorb WHERE cookie_id=\"".$cookie_id."\";";
 $result = mysqli_query($verbinde, $sql);
          
 if (mysqli_num_rows ($result) > 0)
 {   
-    while ($zeile = mysqli_fetch_assoc($result))
-    {
+    //$resultset = array();
+    while ($wkzeile = mysqli_fetch_array($result)) 
 
-        // artikel daten zu den artikel ids aus dem warenkorb suchen
+  
+       // {
+       //     $resultset[]= $zeile;
+      //  }
+     //   foreach ($resultset as $resultrow)
+        {
+        //einträge zählen
+        echo count ($wkzeile);
+
+        // artikel daten zu den artikel IDs aus dem warenkorb suchen
         $sql = "SELECT art_id, art_name, art_preis, art_stueckzahl, art_bild 
-        FROM artikel WHERE art_id=\"".$zeile['art_id']."\";";
+        FROM artikel WHERE art_id=\"".$wkzeile['art_id']."\";";
         $result = mysqli_query($verbinde, $sql);
-
-        //ausgabe nav zeile
-        echo "<div class=\"kasse_headleiste\">";
-        echo "Kasse <br>";
-        echo "<a class=\"kasse_nav\">Adresse </a> <a class=\"kasse_nav\"> Zahlungsmethode </a> <a class=\"kasse_nav_active\"> Kauf abschließen </a> <a class=\"kasse_nav\"> Bestellbestätigung </a>";
-    echo "</div>";
-
-        //linke seite
-        echo "<div class=\"kasse_div\">";
-        echo "<div class=\"kasse_links\">";
-            echo "<span class=\"kasse_ueberschrift_klein\">Überprüfe deine Bestellung und schließe den Kauf ab:</span><br>";
-            echo "<table>";
+        
+    
            
         
     if (mysqli_num_rows ($result) > 0)
@@ -190,7 +244,7 @@ if (mysqli_num_rows ($result) > 0)
              
 //abfrage, ob stückzahl vorhanden
              //stückzahl nicht mehr vorhanden:
-                if ($row['art_stueckzahl']< $zeile['anzahl_art'])
+                if ($row['art_stueckzahl']< $wkzeile['anzahl_art'])
                 {   echo " <form method=\"POST\" action=\"#\"> <tr> ";
                     echo "<tr> ";
                     echo "<td class=\"kasse_td\"> <img class=\"kasse_artikel_bild\" src=\"img/".$row['art_bild']."\"> </td>";
@@ -201,10 +255,10 @@ if (mysqli_num_rows ($result) > 0)
                     <button type=\"submit\" name=\"kasse_3_anzahl_up\"> + </button> 
                     <button type=\"submit\" name=\"kasse_3_anzahl_down\"> - </button> <br> je ".$row['art_preis']." €</td>
                     <input type=\"hidden\" name=\"kasse_art_id\" value=\"".$row['art_id']."\">
-                    <input type=\"hidden\" name=\"kasse_wk_zahl\" value=\"".$zeile['anzahl_art']."\">
+                    <input type=\"hidden\" name=\"kasse_wk_zahl\" value=\"".$wkzeile['anzahl_art']."\">
                     <input type=\"hidden\" name=\"kasse_art_zahl\" value=\"".$row['art_stueckzahl']."\">";
 
-                    echo "<td class=\"kasse_td\">".$row['art_preis'] * $zeile['anzahl_art']." €"; 
+                    echo "<td class=\"kasse_td\">".$row['art_preis'] * $wkzeile['anzahl_art']." €"; 
 
                     echo "</tr>  </form>";
 // Fehlerausgabe
@@ -216,28 +270,28 @@ if (mysqli_num_rows ($result) > 0)
                     echo " <form method=\"POST\" action=\"#\"> <tr> ";
                     echo "<td class=\"kasse_td\"> <img class=\"kasse_artikel_bild\" src=\"img/".$row['art_bild']."\"> </td>";
                     echo "<td class=\"kasse_td\">".$row['art_name']."<br> <button type=\"submit\" name=\"kasse_3_artikel_delete\"> artikel löschen</button> </td>";
-                    echo "<td class=\"kasse_td\">".$zeile['anzahl_art']."
+                    echo "<td class=\"kasse_td\">".$wkzeile['anzahl_art']."
                    
                     <input type=\"hidden\" name=\"kasse_art_id\" value=\"".$row['art_id']."\">
-                    <input type=\"hidden\" name=\"kasse_wk_zahl\" value=\"".$zeile['anzahl_art']."\">
+                    <input type=\"hidden\" name=\"kasse_wk_zahl\" value=\"".$wkzeile['anzahl_art']."\">
                     <input type=\"hidden\" name=\"kasse_art_zahl\" value=\"".$row['art_stueckzahl']."\">
                     <button type=\"submit\" name=\"kasse_3_anzahl_up\"> + </button> <button type=\"submit\" name=\"kasse_3_anzahl_down\"> - </button> 
                     
                     <br> je ".$row['art_preis']." €</td>";
-                    echo "<td class=\"kasse_td\">".$row['art_preis'] * $zeile['anzahl_art']." €";
+                    echo "<td class=\"kasse_td\">".$row['art_preis'] * $wkzeile['anzahl_art']." €";
                     echo "</tr> </form>";
                 }
 
             }
 
-        
         }
+    }
  
 
     }
     echo "</table>";
     echo "<form action=\"#\" method=\"POST\">";
-    echo "<input type=\"checkbox\" id=\"AGB_check\" name=\AGB_check\" oncheck=\"return clickbar()\">
+    echo "<input type=\"checkbox\" id=\"AGB_check\" name=\"AGB_check\" onchange=\"return clickbar()\">
     <span class=\"kasse_rechts_klein\">Ich akzeptiere die <a class=\"kasse_ueberschrift_klein\"> AGB</a>
     und die <a class=\"kasse_ueberschrift_klein\"> Datenschutzbestimmungen </a>
     und bestätige die Korrektheit meiner Daten.</span>";
@@ -245,6 +299,6 @@ if (mysqli_num_rows ($result) > 0)
     echo "<hr>";
 }
 }
-}
+
 
 ?>
