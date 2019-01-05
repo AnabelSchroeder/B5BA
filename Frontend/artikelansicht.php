8be7d307d8869ada2bda7693b99c68186869df13
Lisa Peters <lisa_peters@Lisas-MBP-2.fritz.box>
2019-01-06 00:00:39 +0100

    L: Standardmenge 1 gesetzt; Auskommentiertes DEL

--- Frontend/artikelansicht.php
+++ Frontend/artikelansicht.php
@@ -77,17 +77,20 @@ $dbname = "BA_webshop"; //in ba_webshop ändern bei zusammenführung
                       //ENTWEDER SO ODER BEIM SHOP ARTIKEL MIT MENGE 0 NICHT ANZEIGEN____________!!!!!!!!  
                         if ($row['art_stueckzahl']>0){
                             //Formular für Menge in den Warenkorb
-                            echo "<form action=\"index.php\" method=\"post\">";
+                            echo "<form action=\"index.php?Seiten_ID=warenkorb\" method=\"post\">";
                             echo "<select name=\"Menge\">";
-                            for ($i=1; $i<=$row['art_stueckzahl']; $i++){
-                                echo "<option value=\"".$i."\">".$i."</option>";
+                            echo "<option value=1 selected>1</option>";
+                            for ($i=2; $i<=$row['art_stueckzahl']; $i++){
+                                echo "<option value=".$i.">".$i."</option>";
                             } 
                             echo "</select>";
-                            echo "<button id=\"L_artInBasket\" name=\"".$row['art_id']."\" type=\"submit\">In den Warenkorb</button>";
+                          
+                            echo "<button id=\"L_artInBasket\" name=\"L_artIDinBasket\" ";
+                            echo "value=".$row['art_id']." type=\"submit\">In den Warenkorb</button>";
                             echo "</form>";
                         }else{echo "<p>Wird nachbestellt.</p>";}
                         
-                        
+                        include "Frontend/L_DB_artikelWarenkorb1.php";
 
                  echo "</div>";
 
@@ -158,144 +161,7 @@ $dbname = "BA_webshop"; //in ba_webshop ändern bei zusammenführung
                 echo "</table>";
 
     echo "</div>";
-          //  }       
-
-
-/*
-echo "<div id=\"L_artikelAnsicht\" class=\"L_contentbereich\">";
-
-        //Container für Bild und Kaufinfos -----------------------
-    echo "<div id=\"L_artAnsichtKopfCont\">";
-
-            //Container für Bild
-        echo "<div id=\"L_artBild\">";
-            echo "<img class=\"L_artImg\" src=\"assets/img/begonie.jpg\">";
-        echo "</div>";
-
-            //Container für Kaufinfos
-        echo "<div id=\"L_artKaufInfoCon\">";
-                //Artikelname
-            echo "<h1 class=\"L_h1\">";
-                echo "BEISPIEL";
-            echo "</h1>";
-                //Container für Kaufinfos
-            echo "<div id=\"L_artKaufInfo\">";
-                    //Absatz Preis
-                echo "<p id=\"L_artPreis\">";
-                    echo "12,34 €";
-                echo "</p>";
-
-                echo "<p id=\"L_artInklMWST\">";
-                    echo "inkl. MwSt.";
-                echo "</p>";
-
-
-                echo "<p id=\"L_zzglVersandko\">";
-                    echo "zzgl. Versandkosten";
-                echo "</p>";
-
-                    //Formular für Menge in den Warenkorb
-                echo "<form action=\"index.php\" method=\"post\">";
-                    echo "<input >";
-                    echo "<button id=\"L_artInBasket\" type=\"submit\">In den Warenkorb</button>";
-                echo "</form>";
-
-            echo "</div>";
-
-
-        echo "</div>";
-
-    echo "</div>";
-    // ------------------------------------------------------
-
-    echo "<div id=\"L_artText\">";
-
-            echo "<h3 class=\"L_h3\">Produktbeschreibung</h3>";
-            echo "<p id=\"L_artProdBeschr\" class=\"L_p\">";
-                echo "Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht. 
-                        Die Pflanze ist grü und blüht. Die Pflanze ist grü und blüht.";
-            echo "</p>";
-
-            echo "<h3 class=\"L_h3\">Pflegehinweise</h3>";
-            echo "<p id=\"L_artPflege\" class=\"L_p\">";
-                echo "Regelmäßig mit Wasser gießen und düngen. Regelmäßig mit Wasser gießen und düngen. 
-                        Regelmäßig mit Wasser gießen und düngen. Regelmäßig mit Wasser gießen und düngen. 
-                        Regelmäßig mit Wasser gießen und düngen. Regelmäßig mit Wasser gießen und düngen. 
-                        Regelmäßig mit Wasser gießen und düngen. Regelmäßig mit Wasser gießen und düngen. 
-                        Regelmäßig mit Wasser gießen und düngen. Regelmäßig mit Wasser gießen und düngen.";
-            echo "</p>";
-
-            echo "<h3 class=\"L_h3\">Produkthinweise</h3>";
-
-                echo "<table class=\"L_table\">";
-
-                
-
-                        echo "<tr class=\"L_tr\">";
-
-                            echo "<td>";
-                                echo "Höhe";
-                            echo "</td>";
-
-                            echo "<td class=\"L_artProdHinR\">";
-                                echo "12 cm";
-                            echo "</td>";
-
-                        echo "</tr>";
-
-                        echo "<tr class=\"L_tr\">";
-
-                            echo "<td>";
-                                echo "Standort";
-                            echo "</td>";
-
-                            echo "<td class=\"L_artProdHinR\">";
-                                echo "Halbschatten";
-                            echo "</td>";
-
-                        echo "</tr>";
-
-                        echo "<tr class=\"L_tr\">";
-
-                            echo "<td>";
-                                echo "Farbe";
-                            echo "</td>";
-
-                            echo "<td class=\"L_artProdHinR\">";
-                                echo "Grün";
-                            echo "</td>";
-
-                        echo "</tr>";
-
-                        echo "<tr class=\"L_tr\">";
-
-                            echo "<td>";
-                                echo "Anlass";
-                            echo "</td>";
-
-                            echo "<td class=\"L_artProdHinR\">";
-                                echo "Geburtstag";
-                            echo "</td>";
-
-                        echo "</tr>";
-
-                
-
-                echo "</table>";
-    echo "</div>";
-echo "</div>";
-*/
+          
 
 }
 
